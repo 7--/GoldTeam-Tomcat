1db5f5a4d498798c4b78421e25824e8c2d516ff5
Phil <regop412@gmail.com>
2018-01-15 19:07:48 -0500

    tomcat setup and db connectoin

--- src/main/java/com/revature/App.java
+++ src/main/java/com/revature/App.java
@@ -1,42 +1,38 @@
 package com.revature;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import com.revature.repository.*;
-import com.revature.beans.*;
-
 @SpringBootApplication
 public class App {
 
-	@Autowired
-	UsersDao usersDao;
-	
-	@Autowired
-	QuizDao quizDao;
-	
-	@Autowired
-	QuestionsDao questionsDao;
-	
-	public static void main(String[] args) {
-		SpringApplication.run(App.class, args);
+    // @Autowired
+    // UsersDao usersDao;
+    //
+    // @Autowired
+    // QuizDao quizDao;
+    //
+    // @Autowired
+    // QuestionsDao questionsDao;
 
-	}
-	
-	@Bean
-	public CommandLineRunner runner() {
-		return args -> {
-			
-			List<Users> users =usersDao.findAll();
-			Users myUser = usersDao.findByfname("Tester");
-			System.out.println(users.toString());
-			System.out.println(myUser.toString());
-		};
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+
+    }
+
+    @Bean
+    public CommandLineRunner runner() {
+
+        return args -> {
+
+            // List<Users> users = usersDao.findAll();
+            // Users myUser = usersDao.findByfname("Tester");
+            // System.out.println(users.toString());
+            // System.out.println(myUser.toString());
+        };
+
+    }
 
 }

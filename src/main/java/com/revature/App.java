8dd485cf0d6c769ef86340c1fc9c74994da5dce5
Phil <regop412@gmail.com>
2018-01-17 23:37:25 -0500

    delete hibernate stuff

--- src/main/java/com/revature/App.java
+++ src/main/java/com/revature/App.java
@@ -6,7 +6,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import com.revature.beans.Users;
 import com.revature.repository.QuestionsDao;
 import com.revature.repository.QuizDao;
 import com.revature.repository.UsersDao;
@@ -14,30 +13,29 @@ import com.revature.repository.UsersDao;
 @SpringBootApplication
 public class App {
 
-	@Autowired
-	UsersDao usersDao;
+    @Autowired
+    UsersDao usersDao;
 
-	@Autowired
-	QuizDao quizDao;
+    @Autowired
+    QuizDao quizDao;
 
-	@Autowired
-	QuestionsDao questionsDao;
+    @Autowired
+    QuestionsDao questionsDao;
 
-	public static void main(String[] args) {
-		SpringApplication.run(App.class, args);
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
 
-	}
+    @Bean
+    public CommandLineRunner runner() {
+        return args -> {
 
-	@Bean
-	public CommandLineRunner runner() {
-		return args -> {
-
-			// List<Users> users =usersDao.findAll();
-			// Users myUser = usersDao.findByUserId(1);
-			// Users myUser = usersDao.findByEmail("regop412@gmail.com");
-			// System.out.println(users.toString());
-			// System.out.println(myUser.toString());
-		};
-	}
+            // List<Users> users =usersDao.findAll();
+            // Users myUser = usersDao.findByUserId(1);
+            // Users myUser = usersDao.findByEmail("regop412@gmail.com");
+            // System.out.println(users.toString());
+            // System.out.println(myUser.toString());
+        };
+    }
 
 }

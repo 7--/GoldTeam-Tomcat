b35e341576e39708d67eb573e3dd231eab4a64e8
Emmanuel <emmanuelgeorge93@gmail.com>
2018-01-18 11:01:09 -0500

    Sixth Commit

--- src/main/java/com/revature/repository/UsersDao.java
+++ src/main/java/com/revature/repository/UsersDao.java
@@ -1,5 +1,7 @@
 package com.revature.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -10,4 +12,5 @@ import com.revature.beans.Users;
 public interface UsersDao extends JpaRepository<Users, Integer> {
 	Users findByUserid(int id);
 	Users findByEmail(String email);
+	List<Users> findAllByOrderByCorrectAnswersDesc();
 }
